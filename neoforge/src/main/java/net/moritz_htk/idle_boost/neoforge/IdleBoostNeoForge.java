52d5803820ef3eb6d24245fdf9fa5bc3eea53cbd
simon-mc28 <hartkopfmoritz5@gmail.com>
2024-07-01 10:27:33 +0200

    Fixed bug where the mod is not client side

--- neoforge/src/main/java/net/moritz_htk/idle_boost/neoforge/IdleBoostNeoForge.java
+++ neoforge/src/main/java/net/moritz_htk/idle_boost/neoforge/IdleBoostNeoForge.java
@@ -3,11 +3,12 @@ package net.moritz_htk.idle_boost.neoforge;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.moritz_htk.idle_boost.IdleBoost;
 import net.moritz_htk.idle_boost.config.IBConfigData;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
-@Mod(IdleBoost.MOD_ID)
+@Mod(value = IdleBoost.MOD_ID, dist = Dist.CLIENT)
 public class IdleBoostNeoForge {
     public IdleBoostNeoForge() {
         IdleBoost.init();
